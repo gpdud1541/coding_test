1b790a0e6f3c26b429636a2c9f93826c410ed4e8
hyeyoung <swim070@samwooim.com>
2021-09-15 17:50:27 +0900

    test1 update

--- test1.java
+++ test1.java
@@ -69,3 +69,25 @@ class Solution {
         return answer;
     }
 }
+
+// ---------------------------------------------------------
+// 다른 사람의 풀이
+// ---------------------------------------------------------
+
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder("");
+        int len = s.length();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i], digits[i]);
+        }
+
+        return Integer.parseInt(s);
+    }
+}
